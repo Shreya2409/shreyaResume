54bc1658637c9edee8b6a146249f6ae6c10d7245
Shreya <shreya.stard@gmail.com>
2023-05-05 09:17:31 +0530

    Configure React app for deployment to GitHub Pages

--- src/Components/Works/Works.jsx
+++ src/Components/Works/Works.jsx
@@ -1,5 +1,11 @@
 import React from 'react'
 import './Works.css'
+import Bsw from "../../img/bsw.jpg"
+import haryana from "../../img/haryana.jpg"
+import argos from "../../img/argos.png"
+import accenture from "../../img/accenture.png"
+import wipro from "../../img/wipro.jpg"
+
 
 const Works = () => {
   return (
@@ -8,7 +14,7 @@ const Works = () => {
          <div className="awesome">
                 <span>Works for All these</span>
                 <span>Brands & Clients</span>
-                <spane>Hello Recruiters please finf
+                <spane>Hello Recruiters please find
                     <br />
                     my awsome services are
                     <br/>
@@ -18,6 +24,30 @@ const Works = () => {
                     <button className="button s-button">Hire Me</button>
                    <div className="blur s-blur" style={{ background: "#ABF1FF94" }}>
                 </div>
+            </div>
+
+            {/* right side */}
+
+            <div className="w-right">
+              <div className="w-mainCircle">
+                <div className="w-secCircle">
+                  <img src={Bsw} alt=""/>
+                </div>
+                <div className="w-secCircle">
+                 <img src={haryana} alt="" />
+                </div>
+                <div className="w-secCircle">
+                  <img src={argos} alt=""/>
+                </div>
+                <div class="w-secCircle">
+                  <img src={accenture} alt="" />
+                </div>
+                <div className="w-secCircle">
+                  <img src={wipro} alt="" />
+                </div>
+              </div>
+          {/* background circles */}
+          <div className="w-backCircle blueCircle"></div>
             </div>
     </div>
   )
